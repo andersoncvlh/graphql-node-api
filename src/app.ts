81bdcfc6ca1a414470df054a46006ec446b3db05
anderson.cvlh <anderson.cvlh@gmail.com>
2018-02-18 15:54:57 -0300

    Configuração Express

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,21 @@
-console.log('Hello World');
-console.log('nodemon');
+import * as express from 'express';
+
+class App {
+
+    public express: express.Application;
+
+    constructor() {
+        this.express = express();
+        this.middleware();
+    }
+    
+    private middleware(): void {
+        this.express.use('/hello', (req: express.Request, resp: express.Response, next: express.NextFunction) => {
+            resp.send({
+                hello: 'Hello World!'
+            });
+        });
+    }
+}
+
+export default new App().express;
