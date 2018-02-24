95c685f6fd6a13796f853a27d8982b8df5680864
anderson.cvlh <anderson.cvlh@gmail.com>
2018-02-24 14:17:52 -0300

    Melhorando código servidor

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import * as express from 'express';
+import * as graphqlHTTP from 'express-graphql';
+
+import schema from './graphql/schema'
 
 class App {
 
@@ -8,13 +11,13 @@ class App {
         this.express = express();
         this.middleware();
     }
-    
+
     private middleware(): void {
-        this.express.use('/hello', (req: express.Request, resp: express.Response, next: express.NextFunction) => {
-            resp.send({
-                hello: 'Hello World!'
-            });
-        });
+
+        this.express.use('', graphqlHTTP({
+            schema: schema,
+            graphiql: process.env.NODE_ENV === 'development'
+        }));
     }
 }
 
